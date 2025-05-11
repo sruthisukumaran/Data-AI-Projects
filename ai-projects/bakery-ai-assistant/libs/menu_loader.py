2b35e660560cfa950a5f617371bac32a7a2f574e
sruthisukumaran <sruthisukumaran.e@gmail.com>
2025-05-11 15:17:47 +0400

    Update menu_loader.py

--- ai-projects/bakery-ai-assistant/libs/menu_loader.py
+++ ai-projects/bakery-ai-assistant/libs/menu_loader.py
@@ -1,1 +1,9 @@
-
+import json
+def load_menu():
+    #Open json file in read mode
+    with open("menu.json","r") as f:
+        json_data = json.load(f)
+        menu_text = ""
+        for name, price in json_data.items():
+            menu_text = menu_text + f"- {name} : AED {price}\n"
+        return menu_text
