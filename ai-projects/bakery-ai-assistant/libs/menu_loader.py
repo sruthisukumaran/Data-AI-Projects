bd283f77f18f1b8bee2fe492f808d04af758e0bd
sruthisukumaran <sruthisukumaran.e@gmail.com>
2025-05-11 12:54:58 +0000

    menu loader updated

--- ai-projects/bakery-ai-assistant/libs/menu_loader.py
+++ ai-projects/bakery-ai-assistant/libs/menu_loader.py
@@ -1,9 +1,16 @@
+import os
 import json
+
 def load_menu():
-    #Open json file in read mode
-    with open("menu.json","r") as f:
+    # Get path to the current file (menu_loader.py)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Go up one directory and access menu.json
+    menu_path = os.path.join(current_dir, "..", "menu.json")
+
+    with open(menu_path, "r") as f:
         json_data = json.load(f)
         menu_text = ""
         for name, price in json_data.items():
-            menu_text = menu_text + f"- {name} : AED {price}\n"
+            menu_text += f"- {name} : AED {price}\n"
         return menu_text
